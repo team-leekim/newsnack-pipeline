121defdd6c6fd0a00f429e8a5022dcfdc4c3f210
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-01-27 20:40:22 +0900

    feat: DB 스키마 정의 및 SQLAlchemy 연결 설정

---
+++ src/database/connection.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 1. 엔진 생성
+engine = create_engine(DATABASE_URL, echo=False)
+
+# 2. 세션 팩토리 생성
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
