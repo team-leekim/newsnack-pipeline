ccb9ae7ce8c51999d3d039a7c7ff5071342fd7f8
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-01-28 00:59:34 +0900

    feat: 카테고리 초기 데이터 삽입 코드 추가

--- sql/init_schema.sql
+++ sql/init_schema.sql
@@ -68,3 +68,8 @@ CREATE TABLE IF NOT EXISTS reaction_count (
                                               angry_count INT DEFAULT 0,
                                               empathy_count INT DEFAULT 0
 );
+
+-- [5] 카테고리 초기 데이터 삽입
+INSERT INTO category (name) VALUES
+('정치'), ('경제'), ('사회'), ('국제'), ('문화'), ('IT'), ('스포츠'), ('연예'), ('라이프'), ('기타')
+ON CONFLICT (name) DO NOTHING;
