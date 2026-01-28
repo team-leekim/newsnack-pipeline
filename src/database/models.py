87ff96e1c451f1f7ae8cc76f7c9c49b0a2ce8d39
Taehoon Kim <taehoonkim.dev@gmail.com>
2026-01-28 13:56:23 +0900

    chore: 임포트 방식 변경

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, BigInteger, String, Text, DateTime, ForeignKey, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql import func
 
 Base = declarative_base()
